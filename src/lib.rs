32523eee580e30709f07b46872376dbba890a062
mfournial <mayeul.fournial@outlook.com>
2017-12-06 11:21:16 +0000

    Add integration tests unit to project

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,7 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+pub fn test_next(x: i32) -> i32 {
+	x + 1
+}
---
+++ tests/integration_tests.rs
@@ -0,0 +1,6 @@
+extern crate collections;
+
+#[test]
+fn test_of_it() {
+	assert_eq!(2, collections::test_next(1));
+}
