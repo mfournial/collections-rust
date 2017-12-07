1cd1de6bd4d80968046b304dc2f1bf0db8fc1ae0
mfournial <mayeul.fournial@outlook.com>
2017-12-07 14:04:43 +0000

    Fix pipeline

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,14 @@ pub fn test_next(x: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
+	use super::*;
+	
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
     #[test]
     fn same_as_it() {
-		assert_eq!(2, collections_more::test_next(1));
+		assert_eq!(2, test_next(1));
     }
 }
